01e4503f5051179dd67ea8912f05825cf0cf2885
helenfaust <helenfaust@bk.ru>
2019-05-15 21:26:12 +0200

    Добавлен ContactCreationTests

---
+++ addressbook-web-tests/src/test/java/TRY/addressbook/ContactCreationTests.java
@@ -0,0 +1,150 @@
+package TRY.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ContactCreationTests {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    driver.get("http://localhost/addressbook");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+    initContactCreation(By.linkText("add new"));
+    fillContactData(new ConactData("Вася", "Бублик", "Корочкин", "Булка", "Гн", "Мельница", "Адрес", "11111", "22222", "33333", "44444", "емейл2@mail.ru", "емейл3@mail.ru", "сайт", "5", "August", "2019", "6", "September", "2020", "Доп.адрес", "555555", "Коммент"));
+    submitContactCreation("(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]");
+  }
+
+  private void submitContactCreation(String s) {
+    driver.findElement(By.xpath(s)).click();
+  }
+
+  private void fillContactData(ConactData conactData) {
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).clear();
+    driver.findElement(By.name("firstname")).sendKeys(conactData.getFirstname());
+    driver.findElement(By.name("middlename")).click();
+    driver.findElement(By.name("middlename")).clear();
+    driver.findElement(By.name("middlename")).sendKeys(conactData.getMiddlename());
+    driver.findElement(By.name("lastname")).clear();
+    driver.findElement(By.name("lastname")).sendKeys(conactData.getLastname());
+    driver.findElement(By.name("nickname")).clear();
+    driver.findElement(By.name("nickname")).sendKeys(conactData.getNickname());
+    driver.findElement(By.name("title")).clear();
+    driver.findElement(By.name("title")).sendKeys(conactData.getTitle());
+    driver.findElement(By.name("company")).clear();
+    driver.findElement(By.name("company")).sendKeys(conactData.getCompany());
+    driver.findElement(By.name("address")).clear();
+    driver.findElement(By.name("address")).sendKeys(conactData.getAddress());
+    driver.findElement(By.name("home")).clear();
+    driver.findElement(By.name("home")).sendKeys(conactData.getHome());
+    driver.findElement(By.name("mobile")).clear();
+    driver.findElement(By.name("mobile")).sendKeys(conactData.getMobile());
+    driver.findElement(By.name("work")).clear();
+    driver.findElement(By.name("work")).sendKeys(conactData.getWorkphone());
+    driver.findElement(By.name("fax")).clear();
+    driver.findElement(By.name("fax")).sendKeys(conactData.getFax());
+    driver.findElement(By.name("email2")).click();
+    driver.findElement(By.name("email2")).clear();
+    driver.findElement(By.name("email2")).sendKeys(conactData.getEmail2());
+    driver.findElement(By.name("email3")).click();
+    driver.findElement(By.name("email3")).clear();
+    driver.findElement(By.name("email3")).sendKeys(conactData.getEmail3());
+    driver.findElement(By.name("homepage")).clear();
+    driver.findElement(By.name("homepage")).sendKeys(conactData.getHomepage());
+    driver.findElement(By.name("bday")).click();
+    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(conactData.getBday());
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[7]")).click();
+    driver.findElement(By.name("bmonth")).click();
+    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(conactData.getBmonth());
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Birthday:'])[1]/following::option[42]")).click();
+    driver.findElement(By.name("byear")).click();
+    driver.findElement(By.name("byear")).clear();
+    driver.findElement(By.name("byear")).sendKeys(conactData.getByear());
+    driver.findElement(By.name("aday")).click();
+    new Select(driver.findElement(By.name("aday"))).selectByVisibleText(conactData.getAday());
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Anniversary:'])[1]/following::option[8]")).click();
+    driver.findElement(By.name("amonth")).click();
+    new Select(driver.findElement(By.name("amonth"))).selectByVisibleText(conactData.getAmonth());
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Anniversary:'])[1]/following::option[43]")).click();
+    driver.findElement(By.name("ayear")).click();
+    driver.findElement(By.name("ayear")).clear();
+    driver.findElement(By.name("ayear")).sendKeys(conactData.getAyear());
+    driver.findElement(By.name("address2")).click();
+    driver.findElement(By.name("address2")).clear();
+    driver.findElement(By.name("address2")).sendKeys(conactData.getAddress2());
+    driver.findElement(By.name("phone2")).clear();
+    driver.findElement(By.name("phone2")).sendKeys(conactData.getPhone2());
+    driver.findElement(By.name("notes")).clear();
+    driver.findElement(By.name("notes")).sendKeys(conactData.getNotes());
+  }
+
+  private void initContactCreation(By add_new) {
+    driver.findElement(add_new).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
